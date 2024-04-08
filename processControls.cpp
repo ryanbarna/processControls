6294dde306d11d0985c87849351986a0a84b1a57
ryanbarna <ryjobar@gmail.com>
2024-04-07 22:06:46 -0600

    generated external error

--- processControls.cpp
+++ processControls.cpp
@@ -38,19 +38,25 @@ int main()
     double derivedError = 0.0;
 
     // Iteratively apply the control algorithm
-    // Stop when process is within 0.1 units of the setpoint
+    // Stop when system is within 0.1 units of the setpoint
     while ( abs(setPoint - processMeasurement) > 0.1 )
     {
         double e = measureError( setPoint, processMeasurement );
 
         // Integrate and derive the system error
-        // integratedError = e * dt;
-        // derivedError = e / dt;
+        integratedError = e * dt;
+        derivedError = e / dt;
 
         // Compute and apply controller response to the process measurement
-        double co = controllerOutput( 1.0, 1.0, 0.5, e, integratedError, derivedError ); 
+        double co = controllerOutput( 1.0, 4.0, 0.1, e, integratedError, derivedError ); 
         processMeasurement = processMeasurement + co;
         i++;
+
+        // Introduce external process changes
+        if ( i >= 1 && i <= 8)
+        {
+            processMeasurement += 1;
+        }
 
         // Print
         cout << "Controller Response: " << co << "\n";
